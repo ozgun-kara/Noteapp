1a31505dcd7153b1d9882ae2494f19ef4c00e8bc
ozgun-kara <ozgunkara.devp@gmail.com>
2022-05-09 12:05:18 +0300

    Update main.dart

--- noteapp/lib/main.dart
+++ noteapp/lib/main.dart
@@ -6,12 +6,5 @@ void main() => runApp(const MyApp());
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'NoteApp',
-      home: SplashView(),
-    );
-  }
+
 }
