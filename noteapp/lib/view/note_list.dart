733b38b8e064258095bdc15268351b35e4808793
ozgun-kara <ozgunkara.devp@gmail.com>
2022-05-17 14:01:13 +0300

    Update note_list.dart

--- noteapp/lib/view/note_list.dart
+++ noteapp/lib/view/note_list.dart
@@ -1,7 +1,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:noteapp/view/note_action.dart';
-
+import 'package:noteapp/viewmodel/db_helper.dart';
+import '../model/note.dart';
 
 class NoteList extends StatefulWidget {
   const NoteList({Key? key}) : super(key: key);
@@ -15,11 +16,9 @@ class _NoteListState extends State<NoteList> {
   List<Note> notes = [];
   int count = 0;
 
-  @override
-  void initState() {
-    super.initState();
-    getData();
-  }
+
+
+
 
   void getData() {
     final notesFuture = helper.getNotes();
