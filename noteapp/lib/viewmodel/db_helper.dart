1ec3dcd3a2e1655a5a9142b132b5b2c5ce62211d
ozgun-kara <ozgunkara.devp@gmail.com>
2022-05-18 12:31:17 +0300

    Update db_helper.dart

--- noteapp/lib/viewmodel/db_helper.dart
+++ noteapp/lib/viewmodel/db_helper.dart
@@ -4,9 +4,14 @@ import '../model/note.dart';
 
 class DbHelper {
 
-  static final DbHelper instance = DbHelper._init();
-  DbHelper._init();
-  static Database? _database;
+  String tblNote = "note";
+  String colId = "id";
+  String colTitle = "title";
+  String colDescription = "description";
+  String colPriority = "priority";
+  String colDate = "date";
+
+
 
   Future<Database> get database async {
     if (_database != null) return _database!;
