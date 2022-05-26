af36f5ca0abb74dc2a8f8db257e3815d664fdfd6
ozgun-kara <ozgunkara.devp@gmail.com>
2022-05-26 15:15:03 +0300

    Update db_helper.dart

--- noteapp/lib/viewmodel/db_helper.dart
+++ noteapp/lib/viewmodel/db_helper.dart
@@ -5,6 +5,44 @@ import '../model/note.dart';
 class DbHelper {
 
 
+  String tblNote = "note";
+  String colId = "id";
+  String colTitle = "title";
+  String colDescription = "description";
+  String colPriority = "priority";
+  String colDate = "date";
+
+  static final DbHelper instance = DbHelper._init();
+  DbHelper._init();
+  static Database? _database;
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+
+    _database = await initializeDb('notes.db');
+
+    return _database!;
+  }
+
+  Future<Database> initializeDb(String filePath) async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, filePath);
+    var dbNotes = openDatabase(path, version: 1, onCreate: _createDb);
+    return dbNotes;
+  }
+
+  void _createDb(Database db, int newVersion) async {
+    await db.execute(
+        "CREATE TABLE $tblNote($colId INTEGER PRIMARY KEY AUTOINCREMENT, $colTitle TEXT,"
+        "$colDescription TEXT, $colPriority INTEGER, $colDate TEXT"
+        ")");
+  }
+
+  Future<int> insertNote(Note note) async {
+    final db = await instance.database;
+    var result = await db.insert(tblNote, note.toJson());
+    return result;
+  }
 
   Future<List> getNotes() async {
     List<Note> noteList = [];
