7750ef22bc73bce0a15fed02dd3e0eead7c984d6
ozgun-kara <ozgunkara.devp@gmail.com>
2022-05-30 00:29:28 +0300

    Update db_helper.dart

--- noteapp/lib/viewmodel/db_helper.dart
+++ noteapp/lib/viewmodel/db_helper.dart
@@ -6,6 +6,13 @@ class DbHelper {
 
 
 
+  String tblNote = "note";
+  String colId = "id";
+  String colTitle = "title";
+  String colDescription = "description";
+  String colPriority = "priority";
+  String colDate = "date";
+
   static final DbHelper instance = DbHelper._init();
   DbHelper._init();
   static Database? _database;
