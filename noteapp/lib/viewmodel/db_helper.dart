7f49a87491295e73ccfa668bf6bcc7da8454c759
ozgun-kara <ozgunkara.devp@gmail.com>
2022-10-21 20:56:50 +0300

    Update db_helper.dart

--- noteapp/lib/viewmodel/db_helper.dart
+++ noteapp/lib/viewmodel/db_helper.dart
@@ -4,7 +4,12 @@ import '../model/note.dart';
 
 class DbHelper {
 
-
+  String tblNote = "note";
+  String colId = "id";
+  String colTitle = "title";
+  String colDescription = "description";
+  String colPriority = "priority";
+  String colDate = "date";
 
   static final DbHelper instance = DbHelper._init();
   DbHelper._init();
@@ -51,14 +56,7 @@ class DbHelper {
     return noteList;
   }
 
-  Future<int?> getCount() async {
-    final db = await instance.database;
 
-    var result = Sqflite.firstIntValue(
-        await db.rawQuery("SELECT COUNT(*) FROM $tblNote"));
-
-    return result;
-  }
 
   Future<int> updateNote(Note note) async {
     final db = await instance.database;
