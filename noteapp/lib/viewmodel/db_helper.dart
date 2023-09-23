b2a62817d80929539cecf70a9f600e27fd0b2eff
ozgun-kara <ozgunkara.devp@gmail.com>
2023-09-23 12:54:51 +0300

    Update db_helper.dart

--- noteapp/lib/viewmodel/db_helper.dart
+++ noteapp/lib/viewmodel/db_helper.dart
@@ -15,20 +15,7 @@ class DbHelper {
   DbHelper._init();
   static Database? _database;
 
-  Future<Database> get database async {
-    if (_database != null) return _database!;
 
-    _database = await initializeDb('notes.db');
-
-    return _database!;
-  }
-
-  Future<Database> initializeDb(String filePath) async {
-    final dbPath = await getDatabasesPath();
-    final path = join(dbPath, filePath);
-    var dbNotes = openDatabase(path, version: 1, onCreate: _createDb);
-    return dbNotes;
-  }
 
 
   void _createDb(Database db, int newVersion) async {
