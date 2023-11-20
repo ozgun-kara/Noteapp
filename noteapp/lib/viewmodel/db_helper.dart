e85402664d06caa7e47a8f7310ea883c17fccff6
ozgun-kara <ozgunkara.devp@gmail.com>
2023-11-20 12:28:24 +0300

    Update db_helper.dart

--- noteapp/lib/viewmodel/db_helper.dart
+++ noteapp/lib/viewmodel/db_helper.dart
@@ -43,7 +43,26 @@ class DbHelper {
     return result;
   }
 
+  Future<List> getNotes() async {
+    List<Note> noteList = [];
+    final db = await instance.database;
 
+    var result =
+        await db.rawQuery("SELECT * FROM $tblNote ORDER BY $colDate ASC");
+    for (var element in result) {
+      noteList.add(Note.fromJson(element));
+    }
+    return noteList;
+  }
+
+  Future<int?> getCount() async {
+    final db = await instance.database;
+
+    var result = Sqflite.firstIntValue(
+        await db.rawQuery("SELECT COUNT(*) FROM $tblNote"));
+
+    return result;
+  }
 
   Future<int> updateNote(Note note) async {
     final db = await instance.database;
